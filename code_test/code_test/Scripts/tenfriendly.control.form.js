04f81996b1f060952c765ed1758f130017ebad01
addisonfunk <funk.addison@gmail.com>
2016-09-20 21:06:52 -0400

    Edited main UI components of home index; added an index and js file for forms

--- code_test/code_test/Scripts/tenfriendly.control.form.js
+++ code_test/code_test/Scripts/tenfriendly.control.form.js
@@ -1,5 +1,6 @@
-﻿$(document).ready(function () {
-
+﻿console.log("testing 1"); 
+$(document).ready(function () {
+    console.log("testing 2");
     $("#friendly-test-btn").on("click", function () {
         alert('test');
     });
