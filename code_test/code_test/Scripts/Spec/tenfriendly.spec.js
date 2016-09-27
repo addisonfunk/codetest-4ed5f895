b0952fde161b49eb5c38f4b53cfd5e81ab0659b6
addisonfunk <funk.addison@gmail.com>
2016-09-26 22:39:01 -0400

    Code clean up; test results in console of Form/Index view

--- code_test/code_test/Scripts/Spec/tenfriendly.spec.js
+++ code_test/code_test/Scripts/Spec/tenfriendly.spec.js
@@ -1,8 +1,8 @@
 ﻿var tenFriendly = new function () {
 
 	QUnit.test("ten friendly test", function (assert) {
-		
 		console.log(assert);
+		console.log("jQUnit test results:", assert.test.assertions);
 		function tenFriendlyString(int) {
 			var sum = 0;
 			var result; 
@@ -16,10 +16,18 @@
 			}
 			return result; 
 		}
-		var result = tenFriendlyString('55');
-		console.log(tenFriendlyString('55')); 
-		console.log(result);
-		assert.equal(result, true, "tenFriendlyString('55') equals true");
+		var testOne = tenFriendlyString('55'),
+		    testTwo = tenFriendlyString('3331'),
+			testThree = tenFriendlyString('1111111111'),
+			testFour = tenFriendlyString('321'),
+			testFive = tenFriendlyString(' ');
+
+		assert.equal(testOne, true, "tenFriendlyString('55') equals true");
+		assert.equal(testTwo, true, "tenFriendlyString('3331') equals true");
+		assert.equal(testThree, true, "tenFriendlyString('1111111111') equals true");
+		assert.equal(testFour, false, "tenFriendlyString('321') equals false");
+		assert.equal(testFive, false, "tenFriendlyString(' ') equals false");
+		console.log(testOne, testTwo, testThree, testFour, testFive); 
 	});
 
 };
--- code_test/code_test/Scripts/tenfriendly.control.form.js
+++ code_test/code_test/Scripts/tenfriendly.control.form.js
@@ -41,8 +41,4 @@
         }
         return result; 
     }; 
-
-
-    //TESTING s
-
 }); 
